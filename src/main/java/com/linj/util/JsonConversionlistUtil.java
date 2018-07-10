5cbb70437b961c6f37e6f0a7af8b2a09fd2291bd
linjtest <1293208049@qq.com>
2018-07-10 18:08:38 +0800

    1

--- src/main/java/com/linj/util/JsonConversionlistUtil.java
+++ src/main/java/com/linj/util/JsonConversionlistUtil.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Lists;
 import com.linj.pojo.TreeView;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -62,7 +64,38 @@ public final class JsonConversionlistUtil{
         }
     }
 
+    /**
+     * 后台查询数据转成前台的复杂json树
+     */
+    /*private static JSONObject JsimpleListToJson(List<?> list){
 
+        return null;
+    }*/
+    public static JSONArray simpleListToJson(List<TreeView> menuList){
+        JSONObject jsonObject = new JSONObject();
+        List<TreeView> nodeList = new ArrayList<>();
+        //todo : 这里的循环不是二次方，而且可用通过数据排序进行优化，肯定可以减少循环比对的次数
+        for (TreeView node1 : menuList) {
+            boolean mark = false;
+            for (TreeView node2 : menuList) {
+                //如果node1的节点不为空 并且 node1节点的父id = node2的节点id
+                if (node1.getParentId() != null && node1.getParentId().equals(node2.getId())) {
+                    //判断成功，就说明node1和node2节点是父子关系： node2：父   node1：子
+                    mark = true;//匹配成功，开关打开
+                    //如果node2父节点 的子节点为空，就证明nede2节点没有其他的子节点数据，执行在node2的节点的children字段添加node1
+                    if (node2.getChildren() == null) node2.setChildren(new ArrayList<TreeView>());
+                    node2.getChildren().add(node1);
+                    break;
+                }
+            }
+            if (!mark) {
+                nodeList.add(node1);
+            }
+        }
+        JSONArray jsonArray = JSONArray.fromObject(nodeList);
+
+        return jsonArray;
+    }
 
 
 }
